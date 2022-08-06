c7bf277a8373d0022e3e57357176d0290297d5f5
Dmytro Batarin <dmytro@batarin.dev>
2022-08-06 20:27:33 +0200

    Register admin

---
+++ netlify/functions/register.ts
@@ -0,0 +1,43 @@
+import { Handler } from '@netlify/functions';
+import { GraphQLClient } from 'graphql-request';
+import jwt from 'jsonwebtoken';
+import crypto from 'crypto';
+import { getSdk } from '../common/sdk';
+
+interface AdminRegsiterInput {
+  username: string;
+  password: string;
+}
+
+const handler: Handler = async (event, context) => {
+  const { body } = event;
+  const input: AdminRegsiterInput = JSON.parse(body!).input.admin;
+  const sdk = getSdk(new GraphQLClient('http://localhost:8080/v1/graphql'));
+
+  const password = crypto
+    .pbkdf2Sync(input.password, 'mygreatsaltsecret', 1000, 64, 'sha512')
+    .toString('hex');
+
+  const data = await sdk.InsertAdmin({
+    username: input.username,
+    password,
+  });
+
+  const accessToken = jwt.sign(
+    {
+      'https://hasura.io/jwt/claims': {
+        'x-hasura-allowed-roles': ['admin'],
+        'x-hasura-default-role': 'admin',
+        'x-hasura-user-id': data.insert_admin?.returning[0].id,
+      },
+    },
+    'mygreatjwtsecret'
+  );
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ accessToken: accessToken }),
+  };
+};
+
+export { handler };
